02842811f1223d84d2951c859ddb79d7236a6944
pankithainfo-ship-it <pankithainfo@gmail.com>
2026-09-17 15:01:56 +0530

    removingqualificationremarks

--- src/studentdetails.jsx
+++ src/studentdetails.jsx
@@ -265,7 +265,8 @@ const StudentDetails = ({ user = 'Student', onLogout }) => {
       String(item.id).toLowerCase().includes(searchValue) ||
       item.date.toLowerCase().includes(searchValue) ||
       item.name.toLowerCase().includes(searchValue) ||
-      item.phone.toLowerCase().includes(searchValue)
+      item.phone.toLowerCase().includes(searchValue) ||
+      item.courses.some((course) => course.toLowerCase().includes(searchValue)) 
     )
   })
 
